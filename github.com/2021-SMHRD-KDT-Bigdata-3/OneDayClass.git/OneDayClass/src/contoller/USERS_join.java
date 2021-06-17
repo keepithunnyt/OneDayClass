681a5fc469a6bf697d8f4185175033f019709de1
smhrd <smhrd@211.105.165.117>
2021-06-17 13:35:02 +0900

    -2021.06.17 servlet 수정

--- github.com/2021-SMHRD-KDT-Bigdata-3/OneDayClass.git/OneDayClass/src/contoller/USERS_join.java
+++ github.com/2021-SMHRD-KDT-Bigdata-3/OneDayClass.git/OneDayClass/src/contoller/USERS_join.java
@@ -12,38 +12,39 @@ import javax.servlet.http.HttpServletResponse;
 import DAO.TEACHERSDAO;
 import DAO.USERSDAO;
 
+@WebServlet("/USERS_join")
+public class USERS_join extends HttpServlet {
 
-@WebServlet("/TEACHERS_join")
-public class TEACHERS_join extends HttpServlet {
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		request.setCharacterEncoding("euc-kr");
 
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 		String name = request.getParameter("name");
-		String tel = request.getParameter("tel");
-	
-		
-		
-		TEACHERSDAO t_dao = new TEACHERSDAO();
-		int cnt = t_dao.insert_teachers(id, pw, name, tel);
-		
+
+		int cnt = 0;
+
+		String address = request.getParameter("address");
+		USERSDAO u_dao = new USERSDAO();
+		cnt = u_dao.insert_users(id, pw, name, address);
+
 		if (cnt > 0) {
 			System.out.println("회원가입 성공!");
 			request.setAttribute("id", id);
 			request.setAttribute("pw", pw);
 			request.setAttribute("name", name);
-			request.setAttribute("tel", tel);
-			
-																//로그인 페이지로 이동
-			RequestDispatcher rd = request.getRequestDispatcher("courses.html");
+			request.setAttribute("address", address);
+
+			// 로그인 페이지로 이동
+			RequestDispatcher rd = request.getRequestDispatcher("main.jsp");
 			rd.forward(request, response);
 
 		}else {
 			response.sendRedirect("join.jsp");
-		}	
+		}
+
 	}
 
 }
